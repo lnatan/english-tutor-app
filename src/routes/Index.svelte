2522f5e547e0b1a5f81bac8875b684c9969b4c7a
nata le <lna29ster@gmail.com>
2020-03-03 21:38:41 +0300

    finish sign-in form

--- src/routes/Index.svelte
+++ src/routes/Index.svelte
@@ -5,16 +5,30 @@
 
 <div class="flex flex-col justify-center items-center min-h-screen py-16">
   <div class="rounded border bg-white py-12 px-16 inline-block text-center">
-    <div class="text-black text-xl mb-8">Sign in</div>
+    <div class="text-black text-2xl mb-8">Sign in</div>
     <form action="">
-      <input type="text" class="border rounded pl-2" placeholder="Login" value=""/>
-      <br/>
-      <input type="password" class="border rounded pl-2 mt-4" placeholder="Password"/>
-      <div class="mt-10">
-        <Button type="primary">Log In</Button>
-      </div>      
+      <div class="custom-input mb-8">
+        <span class="icon"><i class="icon-login"></i></span>
+        <input type="text" class="input" placeholder="Login" value="">
+      </div>
+      <div class="custom-input mb-8">
+        <span class="icon"><i class="icon-password"></i></span>
+        <input type="password" class="input" placeholder="Password" value="">
+      </div>
+      <Button type="primary">Log In</Button>     
     </form>
   </div>
   <br/>
   <a href="/lesson" use:link class="text-primary">Enter</a>
 </div>
+
+<style>
+  .custom-input {
+    @apply border border-grey-5 rounded pl-2 py-2;
+  }
+  .input {
+    padding: 0;
+    border: none;
+    outline: none;
+  }
+</style>
